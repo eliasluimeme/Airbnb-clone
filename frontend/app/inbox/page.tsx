eb8c9511cd04de875a1689c51501b3fe94a753fe
Elias <1337isloading@gmail.com>
2024-03-04 13:45:40 +0100

    inbox page && user menu popup

---
+++ frontend/app/inbox/page.tsx
@@ -0,0 +1,15 @@
+import ConversationPage from "../components/inbox/conversation";
+
+const InboxPage = () => {
+    return (
+        <main className="max-w-[1500px] mx-auto px-6 pb-6 space-y-4">
+            <h1 className="my-6 text-2xl">Inbox</h1>
+
+            <ConversationPage />
+            <ConversationPage />
+            <ConversationPage />
+        </main>
+    )
+}
+
+export default InboxPage;   
