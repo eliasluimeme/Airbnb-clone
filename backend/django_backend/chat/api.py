d319714f7de1af6c52182bcd3adc755b5f659169
Elias <1337isloading@gmail.com>
2024-04-23 19:36:22 +0100

    connect sockets

--- backend/django_backend/chat/api.py
+++ backend/django_backend/chat/api.py
@@ -3,10 +3,19 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view
 
 from .models import Conversation
-from .serializers import ConversationListSerializer
+from .serializers import ConversationListSerializer, ConversationDetailSereializer
 
 @api_view(['GET'])
 def conversation_list(request):
     serializer = ConversationListSerializer(request.user.conversations, many=True)
 
     return JsonResponse(serializer.data, safe=False)
+
+@api_view(['GET'])
+def conversation_detail(request, pk):
+    conversation = request.user.conversations.get(pk=pk)
+    serializer = ConversationDetailSereializer(conversation, many=False)
+
+    return JsonResponse({
+        'conversation': serializer.data
+    }, safe=False)
--- frontend/app/inbox/[id]/page.tsx
+++ frontend/app/inbox/[id]/page.tsx
@@ -1,9 +1,41 @@
-import ConversationDetail from "@/app/components/inbox/conversationDetail";
+import { getUserId } from "@/app/lib/actions";
+import React, { useState, useEffect } from "react";
+import apiService from "@/app/services/apiService";
 
-const ConversationPage = () => {
+import ConversationDetail from "@/app/components/inbox/conversationDetail";
+import { getAccessToken } from "@/app/lib/actions";
+import { UserType } from "../page";
+
+export type MessageType = {
+    id: string;
+    name: string;
+    body: string;
+    conversationId: string;
+    sent_to: UserType;
+    created_by: UserType;
+}
+
+const ConversationPage = async ({ params}: { params: { id:string } }) => {
+    const userId = await getUserId();
+    const token = await getAccessToken();
+
+    if (!userId || !token) {
+        return (
+            <main className="max-w-[1500px] mx-auto px-6 pb-6 py-12">
+                <p>You must be logged in to view your inbox.</p>
+            </main>
+        )
+    }
+
+    const conversation = await apiService.get(`/api/chat/${params.id}/`);
+
     return (
         <main className="max-w-[1500px] mx-auto px-6 pb-6">
-            <ConversationDetail />
+            <ConversationDetail
+                userId={userId}
+                token={token}
+                conversation={conversation.conversation}
+            />
         </main>
     )
 }
