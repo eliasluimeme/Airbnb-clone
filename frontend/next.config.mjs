f656a8b3a36744d0558c0c812de460247d420f9f
eliasluimeme <1337isloading@gmail.com>
2024-09-04 16:29:53 +0100

    backend dockerfile update

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -7,9 +7,25 @@ const nextConfig = {
                 hostname: 'localhost',
                 port: '8000',
                 pathname: '/**'
+            },
+            {
+                protocol: 'https',
+                hostname: 'your-production-backend.com',
+                port: '',
+                pathname: '/**'
             }
         ]
-    }
+    },
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: process.env.NODE_ENV === 'production'
+                    ? 'https://your-production-backend.com/api/:path*'
+                    : 'http://localhost:8000/api/:path*',
+            },
+        ];
+    },
 };
 
 export default nextConfig;
