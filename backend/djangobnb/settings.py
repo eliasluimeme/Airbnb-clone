758d28eb19e9c40ed5485b7cff4b75b6d810894d
eliasluimeme <1337isloading@gmail.com>
2024-09-06 15:43:28 +0100

    production ready - static media

--- backend/djangobnb/settings.py
+++ backend/djangobnb/settings.py
@@ -189,9 +189,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+# STATIC_URL = 'static/'
+# MEDIA_URL = 'media/'
+# MEDIA_ROOT = BASE_DIR / 'media'
+
+MEDIA_URL = 'https://airbnb-clone-8nmo.onrender.com/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
