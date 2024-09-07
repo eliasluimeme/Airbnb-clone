85e3c78dad8f7b6e5e00d65a2f6b760ffc16f6ae
eliasluimeme <1337isloading@gmail.com>
2024-09-07 15:44:56 +0100

    cloudinary file storage

--- backend/djangobnb/settings.py
+++ backend/djangobnb/settings.py
@@ -210,8 +210,8 @@ USE_TZ = True
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
